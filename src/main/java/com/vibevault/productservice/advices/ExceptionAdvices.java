45b69100ce372302ee7430b7ac57c61c057aa013
Sparsh <sparsh.raj30@gmail.com>
2026-01-10 00:45:23 +0530

    Fixed incorrect import for AccessDenied Exception
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- src/main/java/com/vibevault/productservice/advices/ExceptionAdvices.java
+++ src/main/java/com/vibevault/productservice/advices/ExceptionAdvices.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.RestClientException;
 
-import java.nio.file.AccessDeniedException;
+import org.springframework.security.access.AccessDeniedException;
 
 @ControllerAdvice
 public class ExceptionAdvices {
