b28ef9689a00f0962db5ed7488aeb42b4d76f84f
Sparsh <sparsh.raj30@gmail.com>
2026-03-15 13:42:03 +0530

    Add response interceptor to inject X-Elastic-Product header for OpenSearch compatibility
    ES 9.x client validates X-Elastic-Product response header which AWS OpenSearch
    doesn't return. Add HttpAsyncClient response interceptor to inject the header.
    
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- src/main/java/com/vibevault/productservice/configurations/ElasticsearchConfig.java
+++ src/main/java/com/vibevault/productservice/configurations/ElasticsearchConfig.java
@@ -1,12 +1,19 @@
 package com.vibevault.productservice.configurations;
 
 import co.elastic.clients.transport.rest5_client.Rest5ClientOptions;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5ClientBuilder;
 import co.elastic.clients.transport.rest5_client.low_level.RequestOptions;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.protocol.HttpContext;
+import org.springframework.boot.elasticsearch.autoconfigure.Rest5ClientBuilderCustomizer;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// Override default ES 9.x versioned content-type headers for AWS OpenSearch 2.x compatibility.
-// OpenSearch rejects "application/vnd.elasticsearch+json; compatible-with=9".
+// ES 9.x Java client is incompatible with AWS OpenSearch 2.x in two ways:
+// 1. Sends versioned content-type "application/vnd.elasticsearch+json; compatible-with=9"
+// 2. Expects "X-Elastic-Product: Elasticsearch" response header
+// This config fixes both by overriding request headers and injecting the product header on responses.
 @Configuration
 public class ElasticsearchConfig {
 
@@ -17,5 +24,22 @@ public class ElasticsearchConfig {
                 .addHeader("Accept", "application/json")
                 .build();
         return new Rest5ClientOptions(requestOptions, false);
+    }
+
+    @Bean
+    Rest5ClientBuilderCustomizer opensearchCompatibilityCustomizer() {
+        return new Rest5ClientBuilderCustomizer() {
+            @Override
+            public void customize(Rest5ClientBuilder builder) {
+                // no-op — required by interface
+            }
+
+            @Override
+            public void customize(HttpAsyncClientBuilder builder) {
+                builder.addResponseInterceptorFirst((HttpResponse response, org.apache.hc.core5.http.EntityDetails entity, HttpContext context) -> {
+                    response.setHeader("X-Elastic-Product", "Elasticsearch");
+                });
+            }
+        };
     }
 }
